2e91e5de2beebe642c3797f55e36c9865b322db1
Bruno Santiago <br.gabriel10@gmail.com>
2021-08-24 16:26:25 +0200

    primeira versao

--- app/controllers/translation_services_controller.rb
+++ app/controllers/translation_services_controller.rb
@@ -2,4 +2,8 @@ class TranslationServicesController < ApplicationController
   def index
     @translation_services = TranslationService.all
   end
+
+  def show
+    @service = TranslationService.find(params[:id])
+  end
 end
