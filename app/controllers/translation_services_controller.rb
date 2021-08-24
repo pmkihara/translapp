a8954528e9c46ada0f87f8eb5d4d0ca09da11246
Patricia Kihara <patricia.kihara@outlook.com>
2021-08-24 12:21:13 -0300

    controller and html created

--- app/controllers/translation_services_controller.rb
+++ app/controllers/translation_services_controller.rb
@@ -4,4 +4,22 @@ class TranslationServicesController < ApplicationController
   def index
     @translation_services = TranslationService.all
   end
+
+  def edit
+    @translation_service = TranslationService.find(params[:id])
+  end
+
+  def update
+    @translation_service = TranslationService.find(params[:id])
+    if @translation_service.update(translation_params)
+      redirect_to task_path, notice: 'Service was created successfully'
+    else
+      render :edit
+  end
+
+  private
+
+  def translation_services_params
+    params.require(:translation_service).permit(:location, :remote, :original_language, :final_language, :description, :price_per_hour, :active, :user_id)
+  end
 end
