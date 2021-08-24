b936da0e49027b25d1c65228b2d34b3c5032df2f
Felipe von Brasche Figueiredo <felipe@vonbrasche.com>
2021-08-24 21:08:24 +0200

    create action on orders controller created

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,16 @@
 class OrdersController < ApplicationController
+  def create
+    @translation_service = TranslationService.find(params[:translation_service_id])
+    @order = Order.new
+    @order.user = current_user
+    @order.translation_service = @translation_service
+    @order.status = "open"
+    @order.date = "hoje"
+    @order.final_cost = @translation_service.price_per_hour
+    if @order.save
+      redirect_to translation_service_order_path(@order)
+    else
+      redirect_to translation_service_path
+    end
+  end
 end
