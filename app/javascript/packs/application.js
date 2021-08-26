cf26a0797b77f188bcec500f9befe59e7eb01a04
Felipe von Brasche Figueiredo <felipe@vonbrasche.com>
2021-08-26 23:49:35 +0200

    transparent navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,10 +28,12 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 import { select2 } from './select2';
+import { initUpdateNavbarOnScroll } from '../components/navbar';
 window.$ = $;
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   select2();
+  initUpdateNavbarOnScroll();
 });
